c2c6a4396dcdee12d71af38861fcb5ed81c67e52
Sam Pedersen <70236468+sampedersen@users.noreply.github.com>
2023-09-26 12:22:09 -0400

    Add comments

--- functions_matlab.m
+++ functions_matlab.m
@@ -25,15 +25,22 @@ Read each function and discern necessary variables needed to be changed.
 
 %}
 
+% Change this to be the directory you want to move 
 source_dir = 'P:\WoodsLab\ACT-head_models\';
+% Change this to be the directory you want to move folder/files into 
 target_dir = 'W:\camctrp\working\';
+% If both of the specifies folders/files exist, try to move them 
 if exist(source_dir) ~= 0 && exist(target_dir) ~= 0
   try 
+    % Move the files 
     movefile(source_dir,target_dir);
+    % Display confirmation that files were moved 
     disp('Moved source folder to target directory successfully.');
   catch 
+    % Notify of error in case unable to move files 
     disp('Failed to move folder.');
   end
+% If both/one of the files/folders DNE, notify of error and double-check inputs. 
 else
   disp('Specified target/source directories do not exist as indicated.');
 end
