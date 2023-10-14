c77f20185485124dc5fe1ce184e53a32aafb903d
Alejandro Albizu <94203469+aa14av@users.noreply.github.com>
2023-10-14 19:15:56 -0400

    new strategy for mismatch filenames

--- getEmpatica.m
+++ getEmpatica.m
@@ -1,27 +1,43 @@
 clear
 
 rootDir = 'P:\WoodsLab\SMART\Data\ACT_E4';
-outDir = fullfile(rootDir,'Organized');
+outDir = fullfile(fileparts(rootDir),'E4_Organized');
 
 if ~exist(outDir,'dir'); mkdir(outDir); end
 zipfdr = dir(fullfile(rootDir,'**','*.csv'));
 
 tic
+missing = cell(length(zipfdr),1);
 parfor i = 1:length(zipfdr)
+    if strcmp(zipfdr(i).folder,outDir); continue; end
+    
     % Get File Parts
     splt = strsplit(zipfdr(i).folder,filesep);
     
     % Get Sub
-    sub = splt{cellfun(@(x) length(x)==6,regexp(splt,'\d','match'))};
+    try 
+        sub = splt{cellfun(@(x) length(x)==6,regexp(splt,'\d','match'))};
+    catch ME
+        missing{i} = zipfdr(i).folder;
+        disp([num2str(i) ': ' ME.message])
+        continue
+    end
     
     % Get Day
     try
         day = splt{~cellfun(@isempty,regexp(splt,'[Dd][Aa][Yy]','match'))};
         day = regexp(day(regexp(day,'[Dd][Aa][Yy]'):end),'\d','match');
         day = [day{:}];
-    catch ME
-        disp(ME.message)
-        continue
+    catch
+        try
+            day = splt{end}; idx = regexp(day,'[_-]');
+            day = regexp(day(idx(end)+1:end),'\d','match');
+            day = [day{:}];
+        catch ME
+            missing{i} = zipfdr(i).folder;
+            disp([sub ': ' ME.message])
+            continue
+        end
     end
     
     % Copy files
