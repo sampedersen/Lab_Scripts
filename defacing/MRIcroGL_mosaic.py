e63117e7bcfe524ebe8df3eb8df7f8ae7d300680
Sam Pedersen <samanthapedersen@ufl.edu>
2023-10-18 12:12:51 -0400

    Declare variables

--- defacing/MRIcroGL_mosaic.py
+++ defacing/MRIcroGL_mosaic.py
@@ -12,3 +12,30 @@ Usage:
     Indicate if JRoast or JBrain should be generated. 
     
 """
+
+# Set these variables
+
+# Set source_folder as the path to the directory containing target participants' data 
+source_folder = "P:\\WoodsLab\\ACT-head_models\\FEM\\Ayden\\deface\\new_montage\\low_group\\"
+# Set output_folder as the path to the directory that images should be exported to 
+output_folder = "P:\\WoodsLab\\ACT-head_models\\FEM\\Ayden\\deface\\new_montage\\sample_imgs\\low\\JRoast\\"
+# List the participants for whom to generate images; must have at least one participant listed, but can be used for batch processing as well 
+# For now, list participants as strings; come back to this 
+participants = ['101190']
+
+
+
+##############################
+# Do not edit 
+
+# File name syntax 
+jroast = "_Jroast.nii"
+jbrain = "_Jbrain.nii"
+all_masks = "_T1orT2_masks.nii"
+elec = "_mask_elec.nii"
+gel = "_mask_gel.nii"
+
+# Algorithm names 
+algorithms = ["original","mri_deface","mideface","fsl_deface","afni_reface","afni_deface"]
+# T1 syntax structures 
+t1s = ["T1","T1_defaced","T1_defaced","T1_defaced","T1.reface","T1.deface"]
