6100ee1fc7634020e19d6a05d7d9df1618c271ab
Sam Pedersen <samanthapedersen@ufl.edu>
2023-10-18 12:22:35 -0400

    Add visualization setting options

--- defacing/MRIcroGL_mosaic.py
+++ defacing/MRIcroGL_mosaic.py
@@ -15,7 +15,7 @@ Usage:
 
 # Set these variables
 
-# Set source_folder as the path to the directory containing target participants' data 
+# Set source_folder as the path to the directory containing target participants' data (NOT THE PARTICIPANTS' ACTUAL FOLDER)
 source_folder = "P:\\WoodsLab\\ACT-head_models\\FEM\\Ayden\\deface\\new_montage\\low_group\\"
 # Set output_folder as the path to the directory that images should be exported to 
 output_folder = "P:\\WoodsLab\\ACT-head_models\\FEM\\Ayden\\deface\\new_montage\\sample_imgs\\low\\JRoast\\"
@@ -23,6 +23,33 @@ output_folder = "P:\\WoodsLab\\ACT-head_models\\FEM\\Ayden\\deface\\new_montage\
 # For now, list participants as strings; come back to this 
 participants = ['101190']
 
+
+
+""" 
+# Identify visualization files 
+
+Indicate with mosaic images to generate. Setting a variable as "True" will generate the images associated. Setting a variable as "False" will skip generating the associated images. 
+    Example: 
+    visualize_JBrain = "True"   ---> Sample images generated using JBrain data 
+    visualize_JRoast = "False"  ---> Skip generating sample images using JRoast data 
+Indicate with visualizations should additionally overlay electrodes/gel to the images. "True" will overlay electrodes/gel, "False" will generate images without electrodes/gel. 
+    Example: 
+    visualize_JRoast = "True"   ---> Sample images will generate using JRoast data
+    overlay_elec_JR = "True"    ---> Sample images will additionally have electrodes and gel overlayed to images 
+    
+"""
+
+# JBrain visualization
+visualize_JBrain = "True"      
+overlay_elec_JB = "True"
+
+# JRoast visualization 
+visualize_JRoast = "True"
+overlay_elec_JR = "True" 
+
+# allMasks visualization 
+visualize_allMasks = "False"
+overlay_elec_AM = "False" 
 
 
 ##############################
