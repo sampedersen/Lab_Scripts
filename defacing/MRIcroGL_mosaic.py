764d497fc11ef78906ad3db69b4f32084c85e99c
Sam Pedersen <samanthapedersen@ufl.edu>
2023-10-18 12:06:22 -0400

    Add file info

---
+++ defacing/MRIcroGL_mosaic.py
@@ -0,0 +1,14 @@
+""" 
+MRIcroGL_mosaic.py
+Author: Sam Pedersen 
+Date: 2023-10-18
+
+Description: 
+    Generate mosaic images of JRoast/JBrain outputs with electrodes overlayed.
+    
+Usage:
+    Indicate if script is to be executed singularly (one participant) or in 
+    batch (multiple participants).
+    Indicate if JRoast or JBrain should be generated. 
+    
+"""
